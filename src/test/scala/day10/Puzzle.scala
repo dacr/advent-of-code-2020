33ff8f9e462b1e1c46e66378e7223a7a924bf88e
Crosson David <crosson.david@gmail.com>
2020-12-10 19:43:55 +0100

    Found Start#2 in an empirical way :( Will have to come back on this puzzle

--- src/test/scala/day10/Puzzle.scala
+++ src/test/scala/day10/Puzzle.scala
@@ -1,6 +1,6 @@
 package day10
 
-// SOLVED IN 1h15m !! not in good shape this morning ! because it was not too hard
+// SOLVED xx
 
 import helpers._
 import org.scalatest.flatspec.AnyFlatSpec
@@ -12,21 +12,48 @@ object PuzzleDay10 {
 
   object Part1 {
 
-
     def solve(input: Iterable[String]): Long = {
-      42
+      val adapters = input.map(_.toInt).to(List)
+      val device = adapters.max + 3 // goal
+      val diffs = (0+:adapters:+device).sorted.sliding(2, 1).map{ case List(a,b) => b-a}.to(List)
+      diffs.count(_ == 1) * diffs.count(_ == 3)
     }
   }
 
   // -------------------------------------------------------------------------
 
   object Part2 {
-    import Part1._
 
-    def solve2(input: Iterable[String]): Long = {
-      42
+    def fact(num: Int): BigDecimal = {
+      (1 to num).map(x => BigDecimal.valueOf(x)).foldLeft(BigDecimal.valueOf(1)) ((a,b) => a * b)
+    }
+
+    // TODO - TO BE REVIEWED AS I FOUND IT in an empirical manner !!!! Thanks to unit tests !
+    def additionsCombinations(n:Int):BigDecimal = {
+      n match {
+        case 2 => 2
+        case 3 => 4
+        case 4 => 7
+      }
+    }
+
+    def solve2(input: Iterable[String]): BigDecimal = {
+      val adapters = input.map(_.toInt).to(List)
+      val goal = adapters.max + 3
+      val diffs = (0+:adapters:+goal).sorted.sliding(2, 1).map{ case List(a,b) => b-a}.to(Array)
+      var consecutives = List.empty[Int]
+      var groupedCount=0
+      for{ i <- 0 until diffs.size} {
+        if (diffs(i)==1) groupedCount+=1
+        else if (diffs(i) != 1 && groupedCount>1) {
+          consecutives=groupedCount::consecutives
+          groupedCount=0
+        } else groupedCount=0
+      }
+      consecutives.map(additionsCombinations).product
     }
   }
+
 }
 
 // =====================================================================================
@@ -37,22 +64,24 @@ class PuzzleDay10Test extends AnyFlatSpec with should.Matchers with Helpers {
 
   "puzzle star#1 example" should "give the right result on the example" in {
     import PuzzleDay10.Part1._
-    solve(resourceContentLines("day10/input-example-1.txt")) shouldBe -1
+    solve(resourceContentLines("day10/input-example-1.txt")) shouldBe 35
+    solve(resourceContentLines("day10/input-example-2.txt")) shouldBe 220
   }
   it should "give the right result on the input file" in {
     import PuzzleDay10.Part1._
-    solve(resourceContentLines("day10/input-given-1.txt")) shouldBe -1
+    solve(resourceContentLines("day10/input-given-1.txt")) shouldBe 2112
   }
 
   // ------------------------------------------------------------------------------------
 
   "puzzle star#2 example" should "give the right result on the example" in {
     import PuzzleDay10.Part2._
-    solve2(resourceContentLines("day10/input-example-1.txt")) shouldBe -1
+    solve2(resourceContentLines("day10/input-example-1.txt")) shouldBe 8
+    solve2(resourceContentLines("day10/input-example-2.txt")) shouldBe 19208
   }
-  it should "give the right result on the input file (functional)" in {
+  it should "give the right result on the input file" in {
     import PuzzleDay10.Part2._
-    solve2(resourceContentLines("day10/input-given-1.txt")) shouldBe -1
+    solve2(resourceContentLines("day10/input-given-1.txt")) shouldBe 3022415986688L
   }
 
 }
