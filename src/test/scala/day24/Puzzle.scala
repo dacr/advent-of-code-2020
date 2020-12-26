7406d4bc402e04d62c04ee000608a00cc85e8118
david crosson <david.crosson@orange.com>
2020-12-26 23:12:45 +0100

    Day24 star#2 easy if instructions are all read carefully !!!

--- src/test/scala/day24/Puzzle.scala
+++ src/test/scala/day24/Puzzle.scala
@@ -28,8 +28,8 @@ object PuzzleDay24 {
   }
 
   val moves:Map[Direction,CubeCoord] = Map(
-    "n"  -> CubeCoord(+0,+0,-1),
-    "s"  -> CubeCoord(+0,+0,+1),
+    //"n"  -> CubeCoord(+0,+0,-1),
+    //"s"  -> CubeCoord(+0,+0,+1),
     "e"  -> CubeCoord(+1,-1,+0),
     "w"  -> CubeCoord(-1,+1,+0),
     "nw" -> CubeCoord(+0,+1,+0),
@@ -64,8 +64,8 @@ object PuzzleDay24 {
         case s"nw$rem" => worker(rem, accu:+moves("nw"))
         case s"se$rem" => worker(rem, accu:+moves("se"))
         case s"sw$rem" => worker(rem, accu:+moves("sw"))
-        case s"n$rem" => worker(rem, accu:+moves("n"))
-        case s"s$rem" => worker(rem, accu:+moves("s"))
+        //case s"n$rem" => worker(rem, accu:+moves("n"))
+        //case s"s$rem" => worker(rem, accu:+moves("s"))
         case s"e$rem" => worker(rem, accu:+moves("e"))
         case s"w$rem" => worker(rem, accu:+moves("w"))
       }
@@ -83,27 +83,28 @@ object PuzzleDay24 {
 
   type Grid=Map[CubeCoord, Color]
 
-  object Part1 {
 
+  @tailrec
+  def fillGrid(remainingMoves:List[Vector[CubeCoord]], grid:Grid):Grid = {
+    remainingMoves match {
+      case Nil => grid
+      case moves::newRemainingMoves =>
+        val cube = moves.reduce(_ add _)
+        val color = grid.getOrElse(cube, White) match {
+          case White => Black
+          case Black => White
+        }
+        val updatedGrid = grid.updated(cube, color)
+        fillGrid(newRemainingMoves, updatedGrid)
+    }
+  }
+
+  // =============================================================================
+
+  object Part1 {
 
     def solve(input: String): Long = {
       val moves = parse(input)
-
-      @tailrec
-      def fillGrid(remainingMoves:List[Vector[CubeCoord]], grid:Grid):Grid = {
-        remainingMoves match {
-          case Nil => grid
-          case moves::newRemainingMoves =>
-            val cube = moves.reduce(_ add _)
-            val color = grid.getOrElse(cube, White) match {
-              case White => Black
-              case Black => White
-            }
-            val updatedGrid = grid.updated(cube, color)
-            fillGrid(newRemainingMoves, updatedGrid)
-        }
-      }
-
       val grid = fillGrid(moves, Map.empty)
       grid.values.count(_ == Black)
     }
@@ -113,9 +114,44 @@ object PuzzleDay24 {
 
   object Part2 {
 
+    //
+    def generateWhiteTilesAroundBlackTiles(grid: Grid):Grid = {
+      def fillWhite(remainingBlacks:List[CubeCoord], grid:Grid):Grid = {
+        remainingBlacks match {
+          case Nil => grid
+          case coord::rems =>
+            val neighbors = moves.values.map(deltaCoord => coord.add(deltaCoord))
+            val whites = neighbors.filterNot(grid.contains).map(coord => coord -> White)
+            val updatedGrid = grid++whites
+            fillWhite(rems, updatedGrid)
+        }
+      }
+      val blacks = grid.collect{case (cube,Black) => cube}.toList
+      fillWhite(blacks, grid)
+    }
+
+    @tailrec
+    def afterGenerations(value: Int, grid: Grid):Grid = {
+      if (value == 0) grid else {
+        val newGrid = grid map {
+          case (coord, White) =>
+            val neighbors = moves.values.map(deltaCoord => coord.add(deltaCoord))
+            val count = neighbors.count(coord => grid.getOrElse(coord, White) == Black)
+            coord -> (if (count==2) Black else White)
+          case (coord, Black) =>
+            val neighbors = moves.values.map(deltaCoord => coord.add(deltaCoord))
+            val count = neighbors.count(coord => grid.getOrElse(coord, White) == Black)
+            coord -> (if (count==0 || count>2) White else Black)
+        }
+        afterGenerations(value - 1, generateWhiteTilesAroundBlackTiles(newGrid))
+      }
+    }
 
     def solve(input: String): Long = {
-      ???
+      val moves = parse(input)
+      val initial = generateWhiteTilesAroundBlackTiles(fillGrid(moves, Map.empty))
+      val grid = afterGenerations(100, initial)
+      grid.values.count(_ == Black)
     }
 
   }
@@ -139,12 +175,12 @@ class PuzzleDay24Test extends AnyFlatSpec with should.Matchers with Helpers {
 
   // ------------------------------------------------------------------------------------
 
-  "puzzle star#2 example" should "give the right result on the example" ignore {
+  "puzzle star#2 example" should "give the right result on the example" in {
     import PuzzleDay24.Part2._
-    solve(resourceContent("day24/input-example-1.txt")) shouldBe 291
+    solve(resourceContent("day24/input-example-1.txt")) shouldBe 2208
   }
-  it should "give the right result on the input file" ignore {
+  it should "give the right result on the input file" in {
     import PuzzleDay24.Part2._
-    solve(resourceContent("day24/input-given-1.txt")) shouldBe 34771
+    solve(resourceContent("day24/input-given-1.txt")) shouldBe 3937
   }
 }
